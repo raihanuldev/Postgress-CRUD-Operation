f821256bcc1818c90d82c202ed527dd235584bcc
UniqueCoderrihan <rihanthedev@gmail.com>
2024-01-24 18:07:22 +0600

    plan

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const PORT = 4770;
 app.listen(PORT,()=>{
     console.log("server is running")
 })
+
+
+
+// PLAN
+
+/**
+ * GET /books => return all books
+ * GET /books/:id => return a specific book
+ * POST /books =>create a book
+ * DELETE /books/:id => delete a book
+ * PUT /books/:id => Update a book
+ * */ 
