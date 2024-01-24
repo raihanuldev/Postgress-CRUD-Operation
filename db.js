421ce709e84e2f91c526f13537ece3160f84913d
UniqueCoderrihan <rihanthedev@gmail.com>
2024-01-25 00:17:24 +0600

    db connect

---
+++ db.js
@@ -0,0 +1,11 @@
+const {Pool} = require('pg');
+
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'database-user',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
+  
