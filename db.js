84be6dcdb089de8e881ce51c4c81dad9cab632d7
UniqueCoderrihan <rihanthedev@gmail.com>
2024-01-25 14:33:37 +0600

    database connected commmand

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'database-user',
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  })
-  
+  host: 'localhost',
+  user: 'postgres',
+  port: 5432,
+  database: "bookdb"
+})
+
+module.exports = pool;
--- db.sql
+++ db.sql
@@ -1,1 +1,10 @@
+-- This is Note For Create A Databse on POSTGRES
+
 CREATE DATABASE bookDB;
+
+
+CREATE TABLE book (
+    id VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(20),
+    description VARCHAR(250)
+);
